dde96060b3d484ea8b0e3a6316b9c5f3a4f72ea9
IanWei <ikaeuu@IanWei.lan>
2018-12-14 15:30:12 +0800

    add styled-component

--- src/components/Profile/Header.js
+++ src/components/Profile/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import bg from '../../assets/images/bg.jpg';
 
 const Header = props => {
+  const style = {
+    background: `url(${bg})`,
+    backgroundSize: "cover",
+    height: "85vh",
+    backgroundImage: `linear-gradient(to right bottom, rgba(0,255,255,.7)`
+  };
   return (
-    <header className="header">
-      
+    <header className="header" style={{...style}}>
+      Hi
     </header>
   );
 };
