7591e1af3e8ded1474890f082c475bafcabf2378
IanWei <ikaeuu@IanWei.lan>
2018-12-14 14:29:48 +0800

    Adding Header

---
+++ src/containers/ProfileBuilder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../components/Profile/Header';
+
+class ProfileBuilder extends Component {
+  render() {
+    return (
+      <Header/>
+    );
+  }
+}
+
+export default ProfileBuilder;
